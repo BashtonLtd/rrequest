52e4e16d8dcef6903828c6d769736346cd5eef37
David Watson <david@bashton.com>
2013-05-24 14:08:45 +0000

    Allow google accounts to be merged with existing account with the same email address.

--- server/models/users.js
+++ server/models/users.js
@@ -19,6 +19,14 @@
  * along with rrequest.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
+Accounts.validateNewUser(function (user) {
+  var existing_user = Meteor.users.findOne({'profile.email':user.profile.email});
+  if (existing_user !== undefined) {
+    throw new Meteor.Error(403, "Email address already in use");
+  }
+  return true;
+});
+
 Accounts.onCreateUser(function(options, user){
   user.profile = options.profile || {};
 
@@ -30,17 +38,29 @@ Accounts.onCreateUser(function(options, user){
     user.profile.email = user.services.google.email;
   }
 
-
   if (!user.profile.name)
     user.profile.name = user.profile.email;
-
 
   // make the first logged in user an admin
   if ( !Meteor.users.find().count() ) {
     user.isAdmin = true;
     user.profile.isStaff = true;
   } else {
+    user.isAdmin = false;
     user.profile.isStaff = false;
+  }
+
+  if (options.profile && options.profile.email) {
+    // Check for existing user
+    var existing_user = Meteor.users.findOne({'profile.email':options.profile.email});
+    if (existing_user !== undefined) {
+      // User with email address already exist
+      user.profile.isStaff = existing_user.profile.isStaff;
+      user.isAdmin = existing_user.isAdmin;
+      user._id = existing_user._id;
+      user.services = _.extend(existing_user.services, user.services);
+      Meteor.users.remove({_id:existing_user._id});
+    }
   }
 
   return user;
