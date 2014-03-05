1a69e84cc9f83dbe4826be9d92a13768ac49b340
David Watson <david@bashton.com>
2014-03-05 15:04:34 +0000

    Update ticket subscriptions.

--- client/views/core/tickets.js
+++ client/views/core/tickets.js
@@ -66,19 +66,12 @@ Template.ticketlist.tickets = function () {
     sortorder = 'desc';
   }
   if (searchfilter == '' || searchfilter == undefined || searchfilter.length < 3) {
-    if (selected_filter_states.length === 0) {
-      tickets = Tickets.find(
-        {},
-        {sort: [[Session.get('selected_sort_field'), sortorder]], limit: ticketListSub.limit()}
-      );
-    } else {
-      tickets = Tickets.find(
-        {
-          status: {$nin: selected_filter_states}
-        },
-        {sort: [[Session.get('selected_sort_field'), sortorder]], limit: ticketListSub.limit()}
-      );
-    }
+    tickets = Tickets.find(
+      {
+        status: {$nin: selected_filter_states}
+      },
+      {sort: [[Session.get('selected_sort_field'), sortorder]], limit: ticketListSub.limit()}
+    );
   } else {
     if (selected_filter_states.length === 0) {
       tickets = Tickets.find(
@@ -138,34 +131,19 @@ var getFilter = function() {
   var searchfilter = Session.get('ticketsSearchfilter');
   var selected_filter_states = Session.get('selected_filter_states');
   if (searchfilter === '' || searchfilter === undefined || searchfilter.length < 3) {
-    if (selected_filter_states.length === 0) {
-      return {};
-    } else {
-      return {
-        status: {$nin: selected_filter_states}
-      };
-    }
+    return {
+      status: {$nin: selected_filter_states}
+    };
   } else {
-    if (selected_filter_states.length === 0) {
-      return {
-        $or:
-        [
-          {_id: {$regex: ".*"+ searchfilter +".*", $options: 'i'}},
-          {subject: {$regex: ".*"+ searchfilter +".*", $options: 'i'}},
-          {'requesters.email': {$regex: ".*"+ searchfilter +".*", $options: 'i'}}
-        ]
-      };
-    } else {
-      return {
-        status: {$nin: selected_filter_states},
-        $or:
-        [
-          {_id: {$regex: ".*"+ searchfilter +".*", $options: 'i'}},
-          {subject: {$regex: ".*"+ searchfilter +".*", $options: 'i'}},
-          {'requesters.email': {$regex: ".*"+ searchfilter +".*", $options: 'i'}}
-        ]
-      };
-    }
+    return {
+      status: {$nin: selected_filter_states},
+      $or:
+      [
+        {_id: {$regex: ".*"+ searchfilter +".*", $options: 'i'}},
+        {subject: {$regex: ".*"+ searchfilter +".*", $options: 'i'}},
+        {'requesters.email': {$regex: ".*"+ searchfilter +".*", $options: 'i'}}
+      ]
+    };
   }
 };
 
